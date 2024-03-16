531f1f4a82a917b68ea51ad7937dc2d10e81bcae
Pär <par.gradin@gmail.com>
2024-03-16 21:51:07 +0100

    initial commit

---
+++ FilterEpg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml;
+
+namespace FilterEpg;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamWriter writer = new StreamWriter("/Users/pargradin/Projects/FilterEpg/FilterEpg/TestData/guidese.xml"))
+        {
+            using (StreamReader reader = new StreamReader("/Users/pargradin/Projects/FilterEpg/FilterEpg/TestData/guide.xml"))
+            {
+                writer.WriteLine(reader.ReadLine());
+
+                // Code to read from the file
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    if (line.Contains(".se\""))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+                reader.Close();
+            }
+
+            writer.WriteLine("</tv>");
+            writer.Close();
+        }
+    }
+}
+
